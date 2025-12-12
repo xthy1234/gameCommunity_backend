801da95323d6d511ebe727b656f021aa0f17f038
xthy1234 <2596892920@qq.com>
2025-12-12 15:59:44 +0800

    功能性测试

---
+++ src/main/java/com/cn/gamecommunity_backend/GameCommunityBackendApplication.java
@@ -0,0 +1,14 @@
+package com.cn.gamecommunity_backend;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.cn.gamecommunity_backend.mapper")
+public class GameCommunityBackendApplication {
+    public static void main(String[] args){
+        SpringApplication.run(GameCommunityBackendApplication.class, args);
+        System.out.println("✅ 后端启动成功！访问：http://localhost:8080");
+    }
+}
---
+++ src/test/java/com/cn/gamecommunity_backend/GamecommunityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.gamecommunity_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamecommunityBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
